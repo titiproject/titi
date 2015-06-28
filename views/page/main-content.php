a0da175c75103fd1288e3bb87d5142c190e8b655
titiproject <htam261@yahoo.com>
2015-06-28 23:22:53 +0700

    acasd

--- views/page/main-content.php
+++ views/page/main-content.php
@@ -42,62 +42,58 @@
     </div><!-- end post -->
     <div class="post" style="">
         <h2><a href="#">Sữa dielac</a><a style="color:#ce9702;" class="more-product" href="#">more products</a></h2>
-        <div class="products">
-            <div class="frame" id="basic" style="overflow: hidden;">
-    <ul class="clearfix" style="transform: translateZ(0px) translateX(-912px); width: 6840px;">
-        <li class="pro">
-            <a href="#"><img src="assets/images/dielac.jpeg"/></a>
-            <p><a href="#">Sữa dinh duong dielac 900g</a></p>
-            <p class="price">Gia : 30.000 VND <a href="#">&nbsp;<i class="fa fa-cart-plus fa-lg"></a></i></p>
-        </li>
-         <li class="pro">
-            <a href="#"><img src="assets/images/dielac.jpeg"/></a>
-            <p><a href="#">Sữa dinh duong dielac 900g</a></p>
-            <p class="price">Gia : 30.000 VND <a href="#">&nbsp;<i class="fa fa-cart-plus fa-lg"></a></i></p>
-        </li>
-         <li class="pro">
-            <a href="#"><img src="assets/images/dielac.jpeg"/></a>
-            <p><a href="#">Sữa dinh duong dielac 900g</a></p>
-            <p class="price">Gia : 30.000 VND <a href="#">&nbsp;<i class="fa fa-cart-plus fa-lg"></a></i></p>
-        </li>
-         <li class="pro">
-            <a href="#"><img src="assets/images/dielac.jpeg"/></a>
-            <p><a href="#">Sữa dinh duong dielac 900g</a></p>
-            <p class="price">Gia : 30.000 VND <a href="#">&nbsp;<i class="fa fa-cart-plus fa-lg"></a></i></p>
-        </li>
-         <li class="pro">
-            <a href="#"><img src="assets/images/dielac.jpeg"/></a>
-            <p><a href="#">Sữa dinh duong dielac 900g</a></p>
-            <p class="price">Gia : 30.000 VND <a href="#">&nbsp;<i class="fa fa-cart-plus fa-lg"></a></i></p>
-        </li>
-         <li class="pro">
-            <a href="#"><img src="assets/images/dielac.jpeg"/></a>
-            <p><a href="#">Sữa dinh duong dielac 900g</a></p>
-            <p class="price">Gia : 30.000 VND <a href="#">&nbsp;<i class="fa fa-cart-plus fa-lg"></a></i></p>
-        </li>
-         <li class="pro">
-            <a href="#"><img src="assets/images/dielac.jpeg"/></a>
-            <p><a href="#">Sữa dinh duong dielac 900g</a></p>
-            <p class="price">Gia : 30.000 VND <a href="#">&nbsp;<i class="fa fa-cart-plus fa-lg"></a></i></p>
-        </li>
-         <li class="pro">
-            <a href="#"><img src="assets/images/dielac.jpeg"/></a>
-            <p><a href="#">Sữa dinh duong dielac 900g</a></p>
-            <p class="price">Gia : 30.000 VND <a href="#">&nbsp;<i class="fa fa-cart-plus fa-lg"></a></i></p>
-        </li>
-         <li class="pro">
-            <a href="#"><img src="assets/images/dielac.jpeg"/></a>
-            <p><a href="#">Sữa dinh duong dielac 900g</a></p>
-            <p class="price">Gia : 30.000 VND <a href="#">&nbsp;<i class="fa fa-cart-plus fa-lg"></a></i></p>
-        </li>
-    </ul>
- </div>
-<div class="controls center" style="margin-top:-150px;">
-    <button class="btn prev" style="float:left;"><i class="fa fa-angle-left fa-3x"></i></button>
-    
-    <button class="btn next" style="float:right;"><i class="fa fa-angle-right fa-3x"></i></button>
-</div>
-            </div>
+        <div class="frame" id="basic" style="overflow: hidden;">
+            <ul class="clearfix" style="transform: translateZ(0px) translateX(-912px); width: 6840px;">
+                <li class="pro">
+                    <a href="#"><img src="assets/images/dielac.jpeg"/></a>
+                    <p><a href="#">Sữa dinh duong dielac 900g</a></p>
+                    <p class="price">Gia : 30.000 VND <a href="#">&nbsp;<i class="fa fa-cart-plus fa-lg"></a></i></p>
+                </li>
+                 <li class="pro">
+                    <a href="#"><img src="assets/images/dielac.jpeg"/></a>
+                    <p><a href="#">Sữa dinh duong dielac 900g</a></p>
+                    <p class="price">Gia : 30.000 VND <a href="#">&nbsp;<i class="fa fa-cart-plus fa-lg"></a></i></p>
+                </li>
+                 <li class="pro">
+                    <a href="#"><img src="assets/images/dielac.jpeg"/></a>
+                    <p><a href="#">Sữa dinh duong dielac 900g</a></p>
+                    <p class="price">Gia : 30.000 VND <a href="#">&nbsp;<i class="fa fa-cart-plus fa-lg"></a></i></p>
+                </li>
+                 <li class="pro">
+                    <a href="#"><img src="assets/images/dielac.jpeg"/></a>
+                    <p><a href="#">Sữa dinh duong dielac 900g</a></p>
+                    <p class="price">Gia : 30.000 VND <a href="#">&nbsp;<i class="fa fa-cart-plus fa-lg"></a></i></p>
+                </li>
+                 <li class="pro">
+                    <a href="#"><img src="assets/images/dielac.jpeg"/></a>
+                    <p><a href="#">Sữa dinh duong dielac 900g</a></p>
+                    <p class="price">Gia : 30.000 VND <a href="#">&nbsp;<i class="fa fa-cart-plus fa-lg"></a></i></p>
+                </li>
+                 <li class="pro">
+                    <a href="#"><img src="assets/images/dielac.jpeg"/></a>
+                    <p><a href="#">Sữa dinh duong dielac 900g</a></p>
+                    <p class="price">Gia : 30.000 VND <a href="#">&nbsp;<i class="fa fa-cart-plus fa-lg"></a></i></p>
+                </li>
+                 <li class="pro">
+                    <a href="#"><img src="assets/images/dielac.jpeg"/></a>
+                    <p><a href="#">Sữa dinh duong dielac 900g</a></p>
+                    <p class="price">Gia : 30.000 VND <a href="#">&nbsp;<i class="fa fa-cart-plus fa-lg"></a></i></p>
+                </li>
+                 <li class="pro">
+                    <a href="#"><img src="assets/images/dielac.jpeg"/></a>
+                    <p><a href="#">Sữa dinh duong dielac 900g</a></p>
+                    <p class="price">Gia : 30.000 VND <a href="#">&nbsp;<i class="fa fa-cart-plus fa-lg"></a></i></p>
+                </li>
+                 <li class="pro">
+                    <a href="#"><img src="assets/images/dielac.jpeg"/></a>
+                    <p><a href="#">Sữa dinh duong dielac 900g</a></p>
+                    <p class="price">Gia : 30.000 VND <a href="#">&nbsp;<i class="fa fa-cart-plus fa-lg"></a></i></p>
+                </li>
+            </ul>
+        </div>
+        <div class="controls center" style="margin-top:-150px;">
+            <button class="btn prev" style="float:left;"><i class="fa fa-angle-left fa-3x"></i></button>
+            <button class="btn next" style="float:right;"><i class="fa fa-angle-right fa-3x"></i></button>
         </div>
     </div><!-- end post -->
 </div><!-- end main content sidebar -->
