8c7fa423f2cc12c56ba9fc9732075df8f815c4d9
titiproject <htam261@yahoo.com>
2015-07-03 23:43:10 +0700

    dadasd

--- controllers/cart/show.php
+++ controllers/cart/show.php
@@ -1,7 +1,7 @@
 <?php
 
-$title = 'Show cart';
+$title = 'Xem giỏ hàng';
 //$_SESSION['total_price'] = calculate_price($_SESSION['cart'],$proList);
 //$_SESSION['item'] = calculate_item($_SESSION['cart']);
-
+$breadcum = 'Xem giỏ hàng';
 require('views/'.$controller.'/'.$action.'.tpl.php');
--- views/cart/show.tpl.php
+++ views/cart/show.tpl.php
@@ -7,14 +7,14 @@
                     <?php require_once 'views/pages/breadcum.php';?>
                     <div id="content">
                         <?php if (array_count_values($_SESSION['cart']) != 0) { ?>
-                        <div class="title">
+                        <div class="carttitle">
                             <ol style="list-style-type: armenian;">
                                 <li>1.Giỏ hàng</li>
                                 <li>2.Thông tin thanh toán</li>
                             </ol>
                         </div>
-                        <div id="cartlist">
-                            <table border="1" bgcolor="#ccc" style="border:1px solid blue;">
+                        <div class="cartlist">
+                            <table border="1" bgcolor="#fff">
                                 <tr>
                                     <th colspan="2" align="left">Tên sản phẩm</th>
                                     <th>Số lượng</th>
@@ -28,23 +28,52 @@
                                 $proItem = $proList->findProductId($k);
                                 echo "<tr>";
                                 echo "<td> <img src='admin/assets/images/".$proItem->getImage()."' height='50' width='50'/></td>";
-                                echo "<td style='vertical-align:top; margin-top:20px;'><a href='index.php?controller=product&action=detail&proid=".$k."'>".$proItem->getProname()."</a></td>";
+                                echo "<td><a href='index.php?controller=product&action=detail&proid=".$k."'>".$proItem->getProname()."</a></td>";
                                 echo "<td>$v</td>";
                                 echo "<td>".number_format($proItem->getPrice())."</td>";
                                 $tt = $v * $proItem->getPrice();
                                 echo "<td>".number_format($tt)."</td>";
-                                echo "<td></td>";
+                                echo "<td><a href='index.php?controller=cart&action=remove&proid=".$proItem->getProid()."'><i class='fa fa-trash fa-lg'></i></a></td>";
                                 echo "</tr>";
                             } ?>
                                 <tr>
-                                    <td colspan="3"><a href="index.php">Tiếp tục mua hàng</a></td>
+                                    <td colspan="3"><a class="continue-cart" href="index.php">Tiếp tục mua hàng</a></td>
                                     <td>Tổng cộng:</td>
-                                    <td><?php echo number_format($_SESSION['total_price']); ?></td>
+                                    <td><span class='price'><?php echo number_format($_SESSION['total_price']); ?></span></td>
                                 </tr>
                             </table>
                         </div>
-                        <div >
-                            
+                        <div id='thanhtoan'>
+                            <table>
+                                <tr>
+                                    <td>Tên khách hàng:</td>
+                                    <td><input type='text'/></td>
+                                </tr>
+                                <tr>
+                                    <td>Tên khách hàng:</td>
+                                    <td><input type='text'/></td>
+                                </tr>
+                                <tr>
+                                    <td>Tên khách hàng:</td>
+                                    <td><input type='text'/></td>
+                                </tr>
+                                <tr>
+                                    <td>Tên khách hàng:</td>
+                                    <td><input type='text'/></td>
+                                </tr>
+                                <tr>
+                                    <td>Tên khách hàng:</td>
+                                    <td><input type='text'/></td>
+                                </tr>
+                                <tr>
+                                    <td>Tên khách hàng:</td>
+                                    <td><input type='text'/></td>
+                                </tr>
+                                <tr>
+                                    <td>Tên khách hàng:</td>
+                                    <td><input type='text'/></td>
+                                </tr>
+                            </table>
                         </div>
                         <?php } else {echo "Khong co san pham trong gio hang"; ?>
                         <table>
